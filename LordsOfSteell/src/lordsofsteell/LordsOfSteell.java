62b4ec15e84a401df221a9349a2500be36a4dcc1
ricar <ricar@DESKTOP-8C202IH>
2023-04-27 10:51:11 +0200

    Primer commit

---
+++ LordsOfSteell/src/lordsofsteell/LordsOfSteell.java
@@ -0,0 +1,15 @@
+
+
+package lordsofsteell;
+import java.util.Scanner;
+/**
+ *
+ * @author ricar
+ */
+public class LordsOfSteell {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+
+}
