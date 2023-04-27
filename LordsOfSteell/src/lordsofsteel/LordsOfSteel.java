bf0ff99f62991b25176883e90920ec85f845b370
ricar <ricar@DESKTOP-8C202IH>
2023-04-27 10:52:45 +0200

    Commit prova

---
+++ LordsOfSteell/src/lordsofsteel/LordsOfSteel.java
@@ -0,0 +1,15 @@
+
+
+package lordsofsteel;
+import java.util.Scanner;
+/**
+ *
+ * @author ricar
+ */
+public class LordsOfSteel {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Me gustan las madalenas");
+    }
+
+}
